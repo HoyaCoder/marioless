680a58d91fa33402c6d6233c1b1df89a6debeba8
HoyaCoder <ghoyas2003@gmail.com>
2017-02-11 15:17:57 -0500

    Create marioless.c

---
+++ marioless.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <cs50.h>
+
+/* Mario less comfortable CS50 project
+   by James Meehan
+*/   
+
+int main(void)
+{
+    
+    int height;
+    do
+    {
+        printf("Height: ");
+        height = get_int();
+    }
+    while (height < 0|| height > 23);
+   
+    for (int i = 0; i < height; i++)
+    {
+    for (int j = 0; j < height - i - 1; j++)
+    {
+        printf("%s", " ");
+    }
+    for (int k = 0; k < i+2; k++)
+        {
+            printf("#");
+        }
+        printf("\n");
+    }
+  
+}
